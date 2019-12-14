c64a896de82c17e6e79b588bd475ba6c58911778
luisantoniococa <luicocurd@gmail.com>
2019-12-14 00:58:25 -0600

    added a quick version fo the tables design, this is the first Draft of the databases desing

---
+++ QuickDBD-export.sql
@@ -0,0 +1,41 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE "Departments" (
+    "dept_no" varchar(4)   NOT NULL,
+    "dept_name" varchar(30)   NOT NULL
+);
+
+-- Table documentation comment 1 (try the PDF/RTF export)
+-- Table documentation comment 2
+CREATE TABLE "dept_emp" (
+    "emp_no" int   NOT NULL,
+    "dept_no" varchar(4)   NOT NULL,
+    "from_date" date   NOT NULL,
+    "to_date" date   NOT NULL,
+    -- Field documentation comment 1
+    -- Field documentation comment 2
+    -- Field documentation comment 3
+    "Name" varchar(200)   NOT NULL,
+    "Price" money   NOT NULL,
+
+    CONSTRAINT "uc_dept_emp_Name" UNIQUE (
+        "Name"
+    )
+);
+
+CREATE TABLE "OrderStatus" (
+    "OrderStatusID" int   NOT NULL,
+    "Name" string   NOT NULL,
+    CONSTRAINT "pk_OrderStatus" PRIMARY KEY (
+        "OrderStatusID"
+     ),
+    CONSTRAINT "uc_OrderStatus_Name" UNIQUE (
+        "Name"
+    )
+);
+
