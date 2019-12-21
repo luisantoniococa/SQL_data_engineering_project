a9b970a9fbb921ad5346fef82cd3a11771e1b534
luisantoniococa <luicocurd@gmail.com>
2019-12-21 11:53:32 -0600

    added queries for the analysis

---
+++ Queries.sql
@@ -0,0 +1,22 @@
+-- we need to get the first name, last name, employer number, gender
+-- and the salary of the employees
+
+SELECT e.emp_no, e.first_name, e.last_name, e.gender, s.salary
+FROM employees e
+JOIN salaries s ON e.emp_no = s.emp_no
+;
+
+-- employees hired in 1986
+
+SELECT e.emp_no, e.first_name, e.last_name, e.hire_date
+FROM employees e
+WHERE e.hire_date between '01/01/1986' AND '01/01/1987'
+;
+
+--manager of each department
+
+SELECT d.dept_no, d.title, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+FROM departments d
+JOIN dept_manager m ON d.dept_no = m.dept_no
+JOIN employees e ON m.emp_no = e.emp_no
+;
