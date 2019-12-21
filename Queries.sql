24636185f2a1d30fa462435bf49b4ee7f7eb22f5
luisantoniococa <luicocurd@gmail.com>
2019-12-21 11:59:03 -0600

    changed the column name of table departments from title to dept_name

--- Queries.sql
+++ Queries.sql
@@ -14,8 +14,9 @@ WHERE e.hire_date between '01/01/1986' AND '01/01/1987'
 ;
 
 --manager of each department
+ALTER TABLE departments RENAME title TO dept_name;
 
-SELECT d.dept_no, d.title, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
+SELECT d.dept_no, d.dept_name, m.emp_no, e.last_name, e.first_name, m.from_date, m.to_date
 FROM departments d
 JOIN dept_manager m ON d.dept_no = m.dept_no
 JOIN employees e ON m.emp_no = e.emp_no
